4a79b575230e48e0eb82986926c9a3024d1f0946
youngGaf <youngabuhafiz@gmail.com>
2020-10-08 15:51:12 +0100

    tweet list view API

--- tweets/views.py
+++ tweets/views.py
@@ -7,10 +7,24 @@ from .models import Tweet
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    returns Json data
+    """
+    querySet = Tweet.objects.all()
+    tweet_list = [{ "id": tweet.id, "content": tweet.content} for tweet in querySet]
+
+    data = {
+        "response": tweet_list,
+        "status": 200,
+    }
+    return JsonResponse(data)
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
-    
+    returns Json data
     """
     data = {"id": tweet_id, }
     try:
