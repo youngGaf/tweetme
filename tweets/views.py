5bd5df4ea7357199dc10de6d8a58c606ac912a5e
youngGaf <youngabuhafiz@gmail.com>
2020-10-08 15:11:42 +0100

    dynamic url setup for tweets

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
+from .models import Tweet
+
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args,kwargs)
+    return HttpResponse("<h1>Hello World</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    
+    """
+
+    data = {"id": tweet_id, }
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+        status = 200
+    except:
+        data['message'] = 'Not found'
+        status = 404
+    return JsonResponse(data, status=status)
