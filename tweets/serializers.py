30d5d9b82fb80922abeea272fa3cb694a289e8d2
youngGaf <youngabuhafiz@gmail.com>
2021-04-22 01:55:15 +0100

    update

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -6,21 +6,26 @@ from .models import Tweet
 MAX_TWEET_LENGTH = settings.MAX_TWEET_LENGTH
 TWEET_ACTIONS_OPTIONS = settings.TWEET_ACTIONS_OPTIONS
 
+
 class TweetActionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     action = serializers.CharField()
+    content= serializers.CharField(allow_blank=True, required=False)
 
     def validate_action(self, action):
         action = action.lower().strip()
         if not action in TWEET_ACTIONS_OPTIONS:
             raise serializers.ValidationError('This is not a valid action for this tweet')
         return action
+
+
 class TweetSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Tweet
-        fields = ['id','content','likes']
-    
+        fields = ['id', 'content', 'likes']
+
     def get_likes(self, obj):
         return obj.likes.count()
 
